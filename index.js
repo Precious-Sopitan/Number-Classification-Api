59b045f59431b0c404fc6c8d129484afa9bfb149
Precious-Sopitan <sopitanprecious1@gmail.com>
2025-02-07 18:01:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/classify-number", async (req, res) => {
+    const { number } = req.query;
+
+    if (!number || isNaN(number)) {
+        return res.status(400).json({ number, error: true });
+    }
+
+    const num = parseInt(number, 10);
+    const properties = [];
+
+    properties.push(num % 2 === 0 ? "even" : "odd");
+
+    
+    const isPrime = num > 1 && [...Array(num).keys()].slice(2).every(i => num % i !== 0);
+    if (isPrime) properties.push("prime");
+
+    
+    const sumDivisors = [...Array(num).keys()].slice(1).filter(i => num % i === 0).reduce((a, b) => a + b, 0);
+    const isPerfect = sumDivisors === num;
+
+    
+    const digits = num.toString().split("");
+    const armstrongSum = digits.reduce((sum, d) => sum + Math.pow(parseInt(d), digits.length), 0);
+    if (armstrongSum === num) properties.push("armstrong");
+
+    
+    let funFact = "No fun fact available.";
+    try {
+        const response = await axios.get(`http://numbersapi.com/${num}?json`);
+        funFact = response.data.text;
+    } catch (error) {
+        console.error("Error fetching fun fact:", error.message);
+    }
+
+    res.json({
+        number: num,
+        is_prime: isPrime,
+        is_perfect: isPerfect,
+        properties,
+        digit_sum: digits.reduce((sum, d) => sum + parseInt(d), 0),
+        fun_fact: funFact
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
